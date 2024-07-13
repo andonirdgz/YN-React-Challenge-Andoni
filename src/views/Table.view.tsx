43d058644fe721c4283a9c105323bee6a0ab2987
andonirdgz <andoni_rdgz@outlook.com>
2024-07-13 16:54:51 -0600

    feat: ✨ Added table with mock data display

--- src/views/Table.view.tsx
+++ src/views/Table.view.tsx
@@ -1,10 +1,19 @@
+import {
+    Table,
+    TableContainer,
+    TableHead,
+    TableBody,
+    TableCell,
+    TableRow,
+    Paper,
+} from '@mui/material'
 import React from 'react'
 
 // TASK 4:
-// - Implement the table from this mockup (public/table_view_mockup.png).
+// - Implement the table from this mockup (public/table_view_mockup.png). ✅
 // - Display answers from store in table.
 // - Each row of the table body should have the name of the answer
-// and its value.
+// and its value. ✅
 // - Add the edit and delete buttons on top of the table.
 
 // TASK 5:
@@ -14,4 +23,52 @@ import React from 'react'
 // - Invoke useResetAnswers hook on delete button click.
 // - See useResetAnswers hook for more guidelines.
 
-export const TableView = () => <div id="table-view"></div>
+const MOCK_ANSWERS = {
+    name: 'John Doe',
+    mail: 'john@doe.com',
+    age: '25',
+    interests: ['Sports', 'Music'],
+}
+
+const COLUMNS = ['Question', 'Answer']
+
+export const TableView = () => {
+    const answers = Object.entries(MOCK_ANSWERS).map(([question, answer]) => {
+        const normalizedQuestion = question.replace('_', ' ')
+
+        if (Array.isArray(answer))
+            return [normalizedQuestion, answer.join(', ')]
+
+        return [normalizedQuestion, answer]
+    })
+
+    return (
+        <div id="table-view">
+            <TableContainer component={Paper}>
+                <Table aria-label="Question answers table">
+                    <TableHead>
+                        <TableRow>
+                            {COLUMNS.map(column => (
+                                <TableCell key={column}>{column}</TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {answers.map(([question, answer]) => (
+                            <TableRow key={question}>
+                                <TableCell
+                                    sx={{
+                                        textTransform: 'capitalize',
+                                    }}
+                                >
+                                    {question}
+                                </TableCell>
+                                <TableCell>{answer}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    )
+}
