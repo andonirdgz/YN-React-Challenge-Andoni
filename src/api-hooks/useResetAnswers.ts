6e3ab74bbe6d9348f54d9733bbfb5b068b715101
andonirdgz <andoni_rdgz@outlook.com>
2024-07-14 13:01:16 -0600

    feat: :sparkles: implemented useResetAnswers hook

--- src/api-hooks/useResetAnswers.ts
+++ src/api-hooks/useResetAnswers.ts
@@ -5,7 +5,17 @@ import { apiToDomainAnswersConverter } from '../api/converters'
 import { useAnswersStore } from '../state'
 
 // TASK 6:
-// - You need to implement a new hook called useResetAnswers.
-// - Once implemented, you should be able to use this hook in the Table view.
+// - You need to implement a new hook called useResetAnswers. ✅
+// - Once implemented, you should be able to use this hook in the Table view. ✅
 
-export const useResetAnswers = () => {}
+export const useResetAnswers = () => {
+    const setAnswers = useAnswersStore(state => state.setAnswers)
+
+    return useMutation({
+        mutationFn: async () => await resetAnswersFromApi(),
+        onSuccess: ({ data }) => {
+            const result = apiToDomainAnswersConverter(data)
+            setAnswers(result)
+        },
+    })
+}
